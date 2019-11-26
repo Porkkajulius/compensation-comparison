97d107c80173bd79ab738a32863e7c3efb95b708
Porkka Julius <julius.porkka@tieto.com>
2019-11-26 23:52:06 +0200

    Modified compensation-service-api structure to new directory

---
+++ compensation-service/employee.py
@@ -0,0 +1,20 @@
+from flask import make_response
+from bson import json_util, ObjectId
+import json
+import pymongo
+
+# MongoDB connection
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["employees"]
+collection = db["employee"]
+
+
+def getAll():
+    myresults = list(collection.find())
+    return json.loads(json_util.dumps(list(collection.find())))
+
+
+def create(employee):
+    return make_response(
+        "Employee successfully created", 201
+    )
