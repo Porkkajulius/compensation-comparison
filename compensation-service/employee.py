cc48c4316a4e15eb71ad0dd3850e9749ee5b77a2
Porkka Julius <julius.porkka@tieto.com>
2020-03-24 16:45:30 +0200

    added new apis for corporate, emplopyee and jobtitle collections

--- compensation-service/employee.py
+++ compensation-service/employee.py
@@ -8,14 +8,29 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["employees"]
 collection = db["employee"]
 
+def listToJson(data):
+    return json.loads(json_util.dumps(list(data)))
 
-def getAll():
-    myresults = list(collection.find())
-    return json.loads(json_util.dumps(list(collection.find())))
+def objectToJson(data):
+    return json.loads(json_util.dumps(data))
 
+def findAll():
+    return listToJson(collection.find())
 
 def create(employee):
     saved_employee = collection.insert_one(employee)
     return make_response(
-        "Employee successfully created", saved_employee, 201
+        "Employee successfully created", objectToJson(saved_employee), 201
     )
+
+def findAllByJobTitle(jobTitle):
+    return listToJson(collection.find({'title': jobTitle}))
+
+# TODO fininalize
+def findMinMedianAndMaxSalaryByJobTitleFromCorporates(jobTitle, corporates):
+    employees = listToJson(collection.find({'title': jobTitle}))
+    employeeDictionary = {}
+    for employee in employees:
+        employeeDictionary.setdefault(employee['company'], []).append(employee['salary'])
+    filteredDictionary = {}
+    return employeeDictionary
---
+++ compensation-service/jobTitle.py
@@ -0,0 +1,27 @@
+from flask import make_response
+from bson import json_util, ObjectId
+import json
+import pymongo
+
+# MongoDB connection
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["employees"]
+collection = db["jobTitle"]
+
+def listToJson(data):
+    return json.loads(json_util.dumps(list(data)))
+
+def objectToJson(data):
+    return json.loads(json_util.dumps(data))
+
+def findAll():
+    return listToJson(collection.find())
+
+def findById(id):
+    return objectToJson(collection.find_one({'_id': ObjectId(id)}))
+
+def create(jobTitle):
+    saved_corporate = corporateCollection.insert_one(jobTitle)
+    return make_response(
+        "Corporate successfully created"#, objectToJson(saved_corporate), 201
+    )
